ba9e2977d7b37df946c386e23fd0a52700dab087
abrar0786 <theibrar1000@gmail.com>
2024-02-11 01:16:10 +0500

    initial commit: Next Course

--- app/flight/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// import { NextResponse } from "next/server";
-const Flight = ({ params }: any) => {
-  return <div>Flight{params.id}</div>;
-};
-
-export default Flight;
--- app/ticket/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Ticket = () => {
-  return <div>Ticket Page </div>;
-};
-export default Ticket;
