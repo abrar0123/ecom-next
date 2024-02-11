3bf69cc00dcc0bb92fef07c4eac26c3d1c437b4a
abrar0786 <theibrar1000@gmail.com>
2024-02-11 11:22:44 +0500

    Frontend : Signup page UI added

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -14,12 +14,46 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="bg-red-100 flex ">
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" />
-      </div>
-    </form>
+    <div className="flex justify-center mt-28">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full sm:w-1/2 lg:w-1/3">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="********"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
