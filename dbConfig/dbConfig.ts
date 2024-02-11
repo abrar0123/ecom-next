62a92c930c67b2abf2caa89974e4a1370f78ee39
abrar0786 <theibrar1000@gmail.com>
2024-02-11 10:52:14 +0500

    Backend : connection

---
+++ dbConfig/dbConfig.ts
@@ -0,0 +1,17 @@
+import mongoose, { Mongoose } from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const Connection = mongoose.connection;
+    Connection.on("connected", () =>
+      console.log("MongoDB Connnection Successfully")
+    );
+    Connection.on("error", (err) => {
+      console.log("MongoDB Connnection Error : ", err);
+      process.exit();
+    });
+  } catch (error) {
+    console.error("Connection Error", error);
+  }
+}
