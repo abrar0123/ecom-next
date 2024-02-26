30507b1ec5b1f1ada7da48585316fa069eef0074
abrar0786 <theibrar1000@gmail.com>
2024-02-27 01:02:34 +0500

    ss

--- src/lib/dbConfig.ts
+++ src/lib/dbConfig.ts
@@ -4,7 +4,6 @@ export async function connectDB() {
   const { DB_USERNAME, DB_PASSWORD } = process.env;
   const connectionStr = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.tz45ju0.mongodb.net/next-ecom`;
   try {
-    console.log("first", DB_USERNAME);
     await mongoose.connect(connectionStr);
     const Connection = mongoose.connection;
 
